220d7aa30b8a6011dbdaa492ace36244b6ea0fe4
Michael Ulrich <msulrich88@gmail.com>
2019-11-07 09:46:04 -0700

    Update is_the_string_balanced.py
    Grammar Corrections

--- is_the_string_balanced.py
+++ is_the_string_balanced.py
@@ -1,53 +1,68 @@
 '''
 Good morning! Here's your coding interview problem for today.
 
-This problem was asked by Amazon.
+This problem was asked by Google.
 
-Given a pivot x, and a list lst, partition the list into three parts.
+You're given a string consisting solely of (, ), and *. * can represent either a (, ), or an empty string. 
 
-    The first part contains all elements in lst that are less than x
-    The second part contains all elements in lst that are equal to x
-    The third part contains all elements in lst that are larger than x
+Determine whether the parentheses are balanced.
 
-Ordering within a part can be arbitrary.
-
-For example, given x = 10 and lst = [9, 12, 3, 5, 14, 10, 10], one partition may be [9, 3, 5, 10, 10, 12, 14].
+For example, (()* and (*) are balanced. )*( is not balanced.
 '''
-
-# MY SOLUTION: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # Start of Program =====================================================================
 
-x = int(input()) # The program asks the user to define the pivot
+String = "((**()(()**)))*)*(()*)" # Random Example String
+print('Given String: ',String)
+print(' ')
 
-Fst = [9, 12, 3, 5, 14, 10, 10] # This is the list to partition as given in the problem
+List = list(String) # Convert the string to a list
 
-# This for loop will give all values in the list less than the pivot to list(A)
-A = [] # Creates empty list A
-for i in Fst:
-  if (i < x):
-    A.append(i)
-    print(A)
+print('String Converted to List: ',List)
+print(' ')
 
-# This for loop will give all values in the list less than the pivot to list(B)
-B = [] # Creates empty list B
-for i in Fst:
-  if (i == x):
-    B.append(i)
-    print(B)
+Length = len(List) # Get the amount of characters in the list to use in the while loop
 
-# This for loop will give all values in the list less than the pivot to list(C)
-C = [] # Creates empty list C
-for i in Fst:
-  if (i > x):
-    C.append(i)
-    print(C)
+print('# of characters in string = ',Length) # See number of characters in string
 
-# The final list is created to list the three partitions in order
-D = [] # Creates empty list D
-D.extend(A)
-D.extend(B)
-D.extend(C)
-print(D)
+leftPar = 0 # Initialize the left parentheses counter
+rightPar = 0 # Initialize the right parentheses counter
+asterisk = 0 # Initialize the asterisk counter
+
+check = 0 # This value will turn to 1 when the imbalance if the imbalance is reached
+
+for i in List: # Iterate over the list
+  print(i) # See which character is being checked in the while loop
+  if i == '(':
+    leftPar += 1 # a += b is shorthand for a = a + b
+    print('leftPar = ',leftPar) # Helps track the count of this character
+    print('asterisk = ',asterisk) # Helps track the count of this character
+  elif i == ')':
+    if leftPar > 0:
+      leftPar -= 1
+      print('leftPar = ',leftPar) # Helps track the count of this character
+      print('asterisk = ',asterisk) # Helps track the count of this character
+    elif asterisk > 0:
+      asterisk -= 1
+      print('leftPar = ',leftPar) # Helps track the count of this character
+      print('asterisk = ',asterisk) # Helps track the count of this character
+    else:
+      check = 1 # The string is determined as imbalanced
+      print('Point of imbalance reached here')
+  elif i == '*':
+    if leftPar > 0:
+      leftPar -= 1
+      print('leftPar = ',leftPar) # Helps track the count of this character
+      print('asterisk = ',asterisk) # Helps track the count of this character
+    else:
+        asterisk += 1
+        print('leftPar = ',leftPar) # Helps track the count of this character
+        print('asterisk = ',asterisk) # Helps track the count of this character
+print(' ')
+
+if check == 0: # The result is printed
+  print('Balanced')
+else:
+  print('Unbalanced')
 
 # End of Program =======================================================================
